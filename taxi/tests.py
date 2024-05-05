34f95d91c5d6906419906f7760e4c739d79a0eeb
skeletronys <mykytcey@gmail.com>
2024-05-06 01:56:59 +0200

    fix flake8

--- taxi/tests.py
+++ taxi/tests.py
@@ -6,24 +6,36 @@ from taxi.models import Manufacturer, Car, Driver
 class TestManufacturers(TestCase):
     def setUp(self) -> None:
         self.manufacturer = Manufacturer.objects.create(
-            name='ManufacturerTest',
-            country='TestUSA'
+            name="ManufacturerTest",
+            country="TestUSA",
         )
 
     def test_manufacturer_str(self):
-        self.assertEqual(str(self.manufacturer), f"{self.manufacturer.name} {self.manufacturer.country}")
+        self.assertEqual(
+            str(self.manufacturer),
+            f"{self.manufacturer.name}"
+            f" {self.manufacturer.country}"
+        )
 
     def test_manufacturer_search_by_name(self):
-        search_results = Manufacturer.objects.filter(name__icontains="nufa")
-        self.assertEqual(search_results.count(), 1)
-        self.assertEqual(search_results.first().name, "ManufacturerTest")
+        search_results = Manufacturer.objects.filter(
+            name__icontains="nufa"
+        )
+        self.assertEqual(
+            search_results.count(),
+            1
+        )
+        self.assertEqual(
+            search_results.first().name,
+            "ManufacturerTest"
+        )
 
 
 class TestCars(TestCase):
     def setUp(self) -> None:
         self.manufacturer = Manufacturer.objects.create(
-            name='ManufacturerTest',
-            country='TestUSA'
+            name="ManufacturerTest",
+            country="TestUSA",
         )
         self.driver = Driver.objects.create_user(
             license_number="ABS23451",
@@ -32,16 +44,29 @@ class TestCars(TestCase):
             last_name="TestLastName",
             password="test1234",
         )
-        self.car = Car.objects.create(model="Test", manufacturer=self.manufacturer)
+        self.car = Car.objects.create(
+            model="Test",
+            manufacturer=self.manufacturer
+        )
         self.car.drivers.set([self.driver])
 
     def test_car_str(self):
-        self.assertEqual(str(self.car), f"{self.car.model}")
+        self.assertEqual(
+            str(self.car),
+            f"{self.car.model}"
+        )
 
     def test_car_search_by_model(self):
-        search_results = Car.objects.filter(model__icontains="st")
-        self.assertEqual(search_results.count(), 1)
-        self.assertEqual(search_results.first().model, "Test")
+        search_results = Car.objects.filter(
+            model__icontains="st"
+        )
+        self.assertEqual(
+            search_results.count(),
+            1)
+        self.assertEqual(
+            search_results.first().model,
+            "Test"
+        )
 
 
 class TestDrivers(TestCase):
@@ -63,7 +88,15 @@ class TestDrivers(TestCase):
         )
 
     def test_driver_search_by_username(self):
-        search_results = Driver.objects.filter(username__icontains="est")
-        self.assertEqual(search_results.count(), 1)
-        self.assertEqual(search_results.first().username, "TestTest")
+        search_results = Driver.objects.filter(
+            username__icontains="est"
+        )
+        self.assertEqual(
+            search_results.count(),
+            1
+        )
+        self.assertEqual(
+            search_results.first().username,
+            "TestTest"
+        )
 
--- taxi/views.py
+++ taxi/views.py
@@ -45,7 +45,9 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ManufacturerListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
-        context["search_form"] = ManufacturerModelSearchForm(initial={"name": name})
+        context["search_form"] = ManufacturerModelSearchForm(
+            initial={"name": name}
+        )
         return context
 
     def get_queryset(self):
@@ -124,7 +126,9 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 
         username = self.request.GET.get("username", "")
 
-        context["search_form"] = DriverUsernameSearchForm(initial={"username": username})
+        context["search_form"] = DriverUsernameSearchForm(
+            initial={"username": username}
+        )
         return context
 
     def get_queryset(self):
